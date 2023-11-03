fa62cc14fc48ba433fe6dabd0635bd5b11ecf29b
kao910019 <kao910019@gmail.com>
2023-11-03 23:49:07 +0800

    Update StreamManager
    Update:
     - remove configs.json
     - Add kline_request command

--- source/streamingManagerService.py
+++ source/streamingManagerService.py
@@ -1,6 +1,7 @@
+import json
 import asyncio
-from typing import Dict
 import multiprocessing as mp
+import numpy as np
 
 from libs.classes.consoleCommandClass import ConsoleCommandClass, ConsoleCommand
 from libs.classes.commandClass import CommandClass, Command
@@ -9,7 +10,6 @@ from libs.configs import Configs
 from libs.serviceBase import ServiceBase
 from libs.utils import *
 
-from streamingListenerService import StreamingListenerService
 from settings import *
 
 
@@ -18,27 +18,17 @@ class StreamingManagerService(ServiceBase):
         super().__init__(configs, "StreamingManager")
 
         self.console.AddModule(self.info["name"], StreamingManagerService, self)
-        self.db.AddModule(self.info["id"], StreamingManagerService, self)
+        self.AddModule(self.info["name"], StreamingManagerService, self)
 
-        self.listeners:Dict[str:dict] = {}
-        
     async def Start(self):
         await super().Start()
 
     async def Close(self):
         await super().Close()
-
-    async def SyncListenerLict(self):
-        self.db.Find()
-
     # ========================================== Console Commands ==========================================
     @ConsoleCommand(command=["stream"], subCommand=["list"], message="stream list : List all listener streams")
     async def StreamList(self, command, subCommand, args):
-        self.streams = {}
-        for clsId in self.listeners.keys():
-            streams = self.db.SendMessage(clsId, "get_streams", ack=True)
-            print(" - {} : {}".format(clsId, streams))
-            self.streams[clsId] = streams
+        pass
     
     @ConsoleCommand(command=["stream"], subCommand=["subscribe", "sub"], message="stream subscribe [streams]: Subscribe streams")
     async def SubscribeStream(self, command, subCommand, args):
@@ -49,15 +39,31 @@ class StreamingManagerService(ServiceBase):
         pass
 
     # ========================================== Service Commands ==========================================
-    @Command("listener_ready")
-    async def ListenerReady(self, cls:str, clsId:str, message:dict = {}):
-        pass
-        # SyncListenerDict
-
-    # @Command("subscribe_streams")
-    # async def SubscribeStreams(self, cls:str, clsId:str, message:dict = {}):
-    #     await self.InitExchange(message["exchange"], message["type"])
-    
-    # @Command("unsubscribe_streams")
-    # async def UnsubscribeStreams(self, cls:str, clsId:str, message:dict = {}):
-    #     await self.InitExchange(message["exchange"], message["type"])
+    # ------------------------------------------
+    # Command : kline_request
+    # Description : Receive request from robot manager service, make sure there're have all kline & listener on the redis
+    # Message : {exchange, type, symbols, periods, count}
+    # -------------------------------------------
+    @Command("kline_request")
+    async def KlineRequest(self, cls:str, clsId:str, message:dict = {}):
+        keys = ["stream:info:{}:{}:{}:{}".format(message["exchange"], message["type"], s, p) for s, p in zip(message["symbols"], message["periods"])]
+        # Get all streaming info on redis
+        result = await self.rs.r.mget(keys)
+        # Check which is not streaming
+        indices = np.where(np.array(result) == None, np.array(result) != None)[0]
+        while len(indices) != 0:
+            outputs = await self.GetServiceList("StreamingListener")
+            for id, info in outputs.items():
+                symbols, periods = np.array(message["symbols"])[indices[:info["freeSubscribe"]]], np.array(message["periods"])[indices[:info["freeSubscribe"]]]
+                msg = {"exchange":message["exchange"], "type":message["type"], "symbols":symbols, "periods":periods, "count":message["count"]}
+                await self.SendMessage(id, "subscribe", msg, ack=True, timeout=60)
+                indices = indices[info["freeSubscribe"]:]
+                if len(indices) == 0:
+                    break
+        # Handler prepare kline
+        await self.SendMessage("StreamingHandler", "kline_prepare", message, ack=True, timeout=60)
+        # Assert kline already finish
+        result = await self.rs.r.mget(keys)
+        indices = np.where(np.array(result) == None, np.array(result) != None)[0]
+        assert (len(indices) == 0)
+        assert all([json.loads(data)["count"] >= message["count"] for data in result])
